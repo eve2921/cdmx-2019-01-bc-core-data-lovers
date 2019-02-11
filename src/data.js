82ee7cb43bc0cc177383d270c47be0373b03cf23
eve2921 <eve2921@hotmail.com>
2019-02-11 11:20:53 -0600

    Actualizando repositorio con master, en CSS ya muestra campeones, filtra, menu aun con fallas

--- src/data.js
+++ src/data.js
@@ -1,9 +1,7 @@
 window.lol = {
- //Itera la data para ingresar a los datos que necesitamos y la devuelve en arreglo
  iterarData: (datalol) => {
-   
-  let datosArr = []
-  for (const key in datalol) {
+   let datosArr = []
+   for (const key in datalol) {
   if (datalol.hasOwnProperty(key)) {
     const element = datalol[key];
     let obj = {
@@ -18,7 +16,6 @@ window.lol = {
   return datosArr
  },
 
- //filtra los roles 
 filtroDataRoles: (rolId, datosArr ) =>{  
      filtradoPorRoles = datosArr.filter(element => element.tag === rolId);
 
